79365b2b6ba8b9834cf8124ef28eefe2469b2c0b
Kenneth Hyman <kenneth.hyman@gmail.com>
2015-11-29 19:13:04 -0500

    Added structure to the first static pages of the site

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = " | 2015 Lit Project"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", full_title("Home")
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", full_title("About")
   end
 
   test "should get bibliography" do
     get :bibliography
     assert_response :success
+    assert_select "title", full_title("Bibliography")
   end
-
+  private
+  def full_title(title)
+    title + @base_title
+  end
 end
